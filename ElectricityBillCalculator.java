31ab7e461f651449c5d6df89a3f2d36ad80225bf
santhosh-hd-ctrl <santhoshhd.in@gmail.com>
2026-03-14 16:34:13 +0530

    Update ElectricityBillCalculator.java

--- ElectricityBillCalculator.java
+++ ElectricityBillCalculator.java
@@ -6,32 +6,7 @@ public class ElectricityBillCalculator {
         
         System.out.print("Enter the number of units consumed: ");
         int units = scanner.nextInt();
-        
-        //double bill = calculateBill(units);
-        
-       // System.out.printf("Total electricity bill for %d units: Rs. %.2f%n", units, bill);
-        
-        //scanner.close();
-   // }
-    
-   // public static double calculateBill(int units) {
-      //  double bill = 0.0;
-        
-       // if (units <= 100) {
-           // bill = units * 5.0;
-      //  } else if (units <= 200) {
-            //bill = 100 * 5.0 + (units - 100) * 7.0;
-       // } else {
-          //  bill = 100 * 5.0 + 100 * 7.0 + (units - 200) * 10.0;
-       // }
-        
-       // return bill;
-
-
-
-       
-        
-        double bill = calculateBill(units);
+          double bill = calculateBill(units);
         
         System.out.printf("Total electricity bill for %d units: Rs. %.2f%n", units, bill);
         
